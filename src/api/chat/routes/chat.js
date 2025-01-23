26d8a9ff73cd2599429b9bd2bfd9b9d27db2456b
Lahari <laharisane1511@gmail.com>
2025-01-23 12:57:30 +0530

    update node engine version and simplify chat routes configuration

--- src/api/chat/routes/chat.js
+++ src/api/chat/routes/chat.js
@@ -8,36 +8,17 @@ const { policy } = require('@strapi/utils');
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreRouter('api::chat.chat', ({ strapi }) => ({
-//   routes: [
-//     {
-//       method: 'GET',
-//       path: '/fetchMessages/:senderId/:receiverId',  // Define the route with params
-//       handler: 'chat.findMessagesBetweenUsers',      // This should match the controller function
-//       config: {
-//         auth: false, // Add authentication if needed
-//       }
-//     }
-//   ]
-// }));
-
 module.exports = {
     routes: [
         {
             method: 'GET',
             path: '/fetchMessages',  // Define the route with params
             handler: 'chat.findMessagesBetweenUsers',      // This should match the controller function
-            config: {
-                // auth: false, // Add authentication if needed
-            }
         },
         {
             method: 'POST',
             path: '/sendMessage',  // The route for adding a message
             handler: 'chat.sendMessage',  // The controller method that handles sending a message
-            config: {
-                // auth: true,  // Optional, if you need authentication for sending messages
-            },
         },
         {
             method: 'POST',
@@ -45,7 +26,6 @@ module.exports = {
             handler: 'chat.checkUserEmail',  // The controller method that handles checking if a user exists
             config: {
                 middlewares: ['global::headerValidation'],
-                // auth: true,  // Optional, if you need authentication for checking if a user exists
             },
         },
     ]
